9bae78ed3489aa164c0a748b4bc4b6068137c8da
Harishrsharma <70387247+Harishrsharma@users.noreply.github.com>
2025-08-17 17:58:59 +0200

    Runnable Primitive in Langchain runnable_branch this runnable primitives have  a conditional sequene of chain

--- Runnables/runnable_lambda.py
+++ Runnables/runnable_lambda.py
@@ -11,7 +11,7 @@ load_dotenv()
 #when creating an OPen source model we have to create llm to choose and initiate the model
 llm =HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1",
-    provider="together"  # specify a provider that actually hosts the model
+    provider="together"  # specify a provider tha t actually hosts the model
 )
 
 model = ChatHuggingFace(llm=llm)
