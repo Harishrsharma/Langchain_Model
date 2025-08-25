e5e40428702bef0140ee050eae0764141d154512
Harishrsharma <70387247+Harishrsharma@users.noreply.github.com>
2025-08-25 20:04:20 +0200

    document loader pypdf_loader

---
+++ Document_loader/pdf_loader.py
@@ -0,0 +1,44 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+
+
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+
+load_dotenv()
+#when creating an OPen source model we have to create llm to choose and initiate the model
+llm =HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    provider="together"  # specify a provider tha t actually hosts the model
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template='Write a summary of this pdf - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs = loader.load()
+
+print(len(docs))
+
+print(docs[0].page_content)
+print(docs[1].metadata)
+
+# creates a chain
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs}))
